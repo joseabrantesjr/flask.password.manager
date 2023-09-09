295b10aaa86d970bd65e8b7098f7673a58a0a751
Jose Calazans Abrantes Junior <130673741+joseabrantesjr@users.noreply.github.com>
2023-09-09 14:11:39 -0300

    update

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
 # The code is importing the `os` module, which provides a way to interact with the operating system.
 import os
 
-# `SECRET_KEY` is a variable that stores a secret key used for cryptographic operations. In this case,
-# it is a byte string represented by `b'1_u7ZjNGVIl6NFYqFPSidZCEI9VuPunU0ha24Ttt7Ho='`. This key is
-# typically used for securing sensitive data, such as user sessions or passwords.
-SECRET_KEY = b'1_u7ZjNGVIl6NFYqFPSidZCEI9VuPunU0ha24Ttt7Ho='
+# Run the secret.key.generate.py script to generate a new secret key.
+SECRET_KEY = b' '
 SQLALCHEMY_DATABASE_URI = 'sqlite:///passwords.db'
